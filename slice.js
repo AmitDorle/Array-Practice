25b339b61bc327a105a9540e1a88cd355781451d
Amit T. Dorle <Amitdorle111@gmail.com>
2022-10-20 10:29:57 +0530

    slice added

---
+++ slice.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple" , "Banana" , "Orange" , "Mango" , "Watermelon"]
+console.log(fruits);
+let sli = fruits.slice(2);
+console.log(sli);
+
+// Slice method slice out a piece of array into new array.It cant change the original array
