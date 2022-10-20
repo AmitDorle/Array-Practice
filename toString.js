5a9d69ce1e990dd46ce7e19ed2f66f5eb8d9c4cf
Amit T. Dorle <Amitdorle111@gmail.com>
2022-10-20 10:38:52 +0530

    toString Added

--- toString.js
+++ toString.js
@@ -1,4 +1,4 @@
-const fruits = ["Apple" , "Banana" , "Orange" , "Mango" , "Watermelon"]
+const fruits = ["Apple" , "Banana" , "Orange" , "Mango" , "Watermelon" , "lemon"]
 console.log(fruits);
 let a = fruits.toString();
 console.log(a);
