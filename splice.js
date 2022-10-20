5843dbdce8997691ade185ca647dcd699d9d31c8
Amit T. Dorle <Amitdorle111@gmail.com>
2022-10-20 10:26:21 +0530

    splice added

--- splice.js
+++ splice.js
@@ -1,4 +1,10 @@
 const fruits = ["Apple" , "Banana" , "Orange" , "Mango" , "Watermelon"]
 console.log(fruits);
-fruits.shift();
+
+// splice is the method by using we add and delete Elements from array at one time it contains three parameters
+// 1)its indicate the index from which the elements are delete or added
+// 2)its number of elements we want to delete
+// 3)elements we want to add into the array
+
+fruits.splice(2, 2, "Aalu");
 console.log(fruits);
