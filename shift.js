e53f5442d6b55b255f4bc7976c81962e18479a04
Amit T. Dorle <Amitdorle111@gmail.com>
2022-10-20 10:37:02 +0530

    shift added

--- shift.js
+++ shift.js
@@ -1,4 +1,4 @@
-const fruits = ["Apple" , "Banana" , "Orange" , "Mango" , "Watermelon"]
+const fruits = ["Apple" , "Banana" , "Orange" , "Mango"]
 console.log(fruits);
 fruits.shift();
 console.log(fruits);
